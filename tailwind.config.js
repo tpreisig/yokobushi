7ede70178ea005a3d91694a886cee4b5d52ea011
Tiziano Preisig <tiziano.preisig@hispeed.ch>
2024-11-19 22:07:28 +0100

    Added all modifications

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./build/*.html'],
+  theme: {
+    extend: {
+      backgroundImage: {
+        'custom': 'linear-gradient(to right, #fff 2px, transparent 2px), linear-gradient(to top, #fff 2px, transparent 2px);',
+      },
+      backgroundSize: {
+        'size': '600px 600px',
+      },
+    },
+  },
+  plugins: [],
+}
+
